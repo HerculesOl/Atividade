99c7272319967db0839c7caf0bd1b39a48fabb90
HerculesOl <147669122+HerculesOl@users.noreply.github.com>
2024-10-04 21:15:03 -0300

    final

---
+++ script.js
@@ -0,0 +1,104 @@
+
+
+
+
+// var num=2
+//     if (num % 2 == 0) {
+//         console.log(`${numero} é par.`);
+//     } else {
+//         console.log(`${numero} é ímpar.`);
+//     }
+
+// var real=0,dolar = 5.46
+// real = Number(prompt("Quanto você quer converter"))
+// real = real / dolar
+// document.write(real+" Esse é o total de dolares")
+
+// let dia= Number(prompt("Digite um numero entre 1 e 7"))
+
+// switch(dia){
+//     case 1:
+//         document.write("Hoje é domingo")
+//         break
+//     case 2:
+//         document.write("Hoje é segunda")
+//         break
+//     case 3:
+//         document.write("Hoje é terça")
+//         break
+//     case 4:
+//         document.write("Hoje é quarta")
+//         break
+//     case 5:
+//         document.write("hoje é quinta")
+//         break
+//     case 6:
+//         document.write("Hoje é sexta")
+//         break
+//     case 7:
+//         document.write("Hoje é sábado")
+//         break
+//     default:
+//         document.write("Numero invalido")
+// }
+
+// var mes = prompt("Digite o nome de um mês")
+
+// switch(mes){
+//     case "janeiro":
+//         document.write(1)
+//         break
+//     case "maio":
+//         document.write(2)
+//         break
+//     case  "março":
+//         document.write(3)
+//         break
+//     case  "abril":
+//         document.write(4)
+//         break
+//     case  "maio":
+//         document.write(5)
+//         break
+//     case  "junho":
+//         document.write(6)
+//         break
+//     case  "julho":
+//         document.write(7)
+//         break
+//     case "agosto":
+//         document.write(8)
+//         break
+//     case  "setembro":
+//         document.write(9)
+//         break
+//     case  "outubro":
+//         document.write(10)
+//         break
+//     case "novembro":
+//         document.write(11)
+//         break
+//     case "dezembro":
+//         document.write(12)
+//         break
+//         default:
+//             document.write("Nome invalido")    
+
+// }
+
+// let horas = Number(prompt("Digite suas horas trabalhadas"))
+// let salario
+// if(horas <= 40){
+//     salario = 10
+//     salario = salario * horas
+//     document.write("seu salario é R$: "+salario)
+// }else if((horas> 40) && (horas <=60)){
+// salario = 400
+// horas = horas - 40
+// horas = horas * 15
+// salario = salario + horas
+// document.write("Seu salario é R$: "+salario)
+// }else{
+//     salario = 400 + 20*20 + (horas-60)*25
+//     document.write("Seu salario é R$: "+salario)    
+// }
